4ec040f8a6e07b929c1fc6687a6ea1f78b2ad49d
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2023-09-01 00:28:23 +0100

    Fix: change ft_lstlast types

--- lst_operations.h
+++ lst_operations.h
@@ -6,7 +6,7 @@
 /*   By: afatimi <afatimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 22:01:05 by afatimi           #+#    #+#             */
-/*   Updated: 2023/09/01 00:09:43 by afatimi          ###   ########.fr       */
+/*   Updated: 2023/09/01 00:16:43 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ t_env	*ft_lstnew(char *elem, t_env *prev);
 void	ft_lstadd_back(t_env **lst, t_env *new);
 int	ft_lstsize(t_env *lst);
 void	ft_lstdelone(t_env *lst, void (*del)(void *));
-t_list	*ft_lstlast(t_list *lst);
+t_env	*ft_lstlast(t_env *lst);
 int	ft_lstsize(t_env *lst);
 # endif
