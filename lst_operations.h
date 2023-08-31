f6cd50c089aa036f0f60ae3ac48739e16d338e8d
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2023-09-01 00:09:54 +0100

    Fix: inclusion

--- lst_operations.h
+++ lst_operations.h
@@ -6,12 +6,13 @@
 /*   By: afatimi <afatimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 22:01:05 by afatimi           #+#    #+#             */
-/*   Updated: 2023/08/31 23:35:03 by afatimi          ###   ########.fr       */
+/*   Updated: 2023/09/01 00:09:43 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LST_OPEATIONS
 # define LST_OPEATIONS
+# include <libft.h>
 
 t_env	*ft_lstnew(char *elem, t_env *prev);
 void	ft_lstadd_back(t_env **lst, t_env *new);
