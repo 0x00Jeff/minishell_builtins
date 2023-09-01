f078d06790bd4c1b3a49e1bfe9100329eeffcfb0
0x00Jeff <jeff_septiceye@protonmail.com>
2023-09-01 01:23:11 +0100

    Fix: pushed the linux version

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: afatimi <afatimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 22:27:45 by afatimi           #+#    #+#             */
-/*   Updated: 2023/08/31 22:54:26 by afatimi          ###   ########.fr       */
+/*   Updated: 2023/09/01 01:22:49 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,18 @@ char	**consume_env(t_env *env)
 {
 	size_t	i;
 	char	**ptr;
+	size_t size;
 	t_env	*node;
 	if (!env)
 		return (NULL);
 
-	ptr = (char **)malloc(ft_lstsize(env) + 1);
+	size = ft_lstsize(env);
+	ptr = (char **)malloc(size + 1);
 	node = env;
 	i = 0;
-	while (i < 0)
+	while (i < size)
 	{
-		ptr[i] = node -> value;
+		ptr[i++] = node -> value;
 		node = node -> next;
 	}
 	ptr[i] = 0;
