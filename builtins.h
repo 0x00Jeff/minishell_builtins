00b6a127e89ee36346f24c326ab235e9d7fbff22
0x00Jeff <jeff_septiceye@protonmail.com>
2023-09-01 02:30:38 +0100

    Feat: global header file

--- builtins.h
+++ builtins.h
@@ -8,8 +8,11 @@ void	cd(char *arg, t_env *env);
 void	my_exit(char *arg);
 void	export(int argc, char **argv, t_env **env);
 void	unset(int argc, char **args, t_env **env);
-void	append_to_env(t_env **env, char *value);
-void	edit_env(t_env *node, char *value);
 void	env_(t_env **env);
 void	pwd();
+t_env	*create_env(char **envp);
+t_env	*search_in_env(t_env *env, char *key);
+char *pwd_trolling(char *str);
+int	check_builtins(int argc, char *command, char **args, t_env **env);
+char	**consume_env(t_env *env);
 #endif
