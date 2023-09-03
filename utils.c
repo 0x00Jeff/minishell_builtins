dec3ca27471b23e0e7319ea58904f785fb457ab4
0x00Jeff <jeff_septiceye@protonmail.com>
2023-09-03 13:06:32 +0100

    chore: delete debug prints

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: afatimi <afatimi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 22:27:45 by afatimi           #+#    #+#             */
-/*   Updated: 2023/09/03 13:04:26 by afatimi          ###   ########.fr       */
+/*   Updated: 2023/09/03 13:06:18 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ char	**consume_env(t_env *env)
 		return (NULL);
 
 	size = ft_lstsize(env);
-	printf("size = %ld\n", size);
 	ptr = (char **)malloc((size + 1 ) * sizeof(char *));
 	node = env;
 	i = 0;
@@ -34,7 +33,6 @@ char	**consume_env(t_env *env)
 		ptr[i++] = node -> value;
 		node = node -> next;
 	}
-	printf("i = %ld\n", i);
-//	ptr[i] = 0;
+	ptr[i] = 0;
 	return (ptr);
 }
