b38a4ab9dadc17b155a6fc2299bf9848b3d91bd0
0x00Jeff <jeff_septiceye@protonmail.com>
2023-09-24 22:17:38 +0100

    feat: retarded log_last_command

--- builtin_utils.c
+++ builtin_utils.c
@@ -6,11 +6,12 @@
 /*   By: ylyoussf <ylyoussf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 22:49:12 by afatimi           #+#    #+#             */
-/*   Updated: 2023/09/24 21:46:28 by afatimi          ###   ########.fr       */
+/*   Updated: 2023/09/24 22:14:26 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin_utils.h"
+#include "builtins.h" // delete thi after figuring out an actual solution for log_last_command
 #include "lst_operations.h"
 #include <libft.h>
 #include <stdio.h>
@@ -414,4 +415,16 @@ t_env	*get_envp(t_env *envp)
 	if (envp)
 		get_envp_internal(envp);
 	return (*get_envp_internal(NULL));
+}
+
+void log_last_command(char *value)
+{
+	char *arr[2];
+	if (!value)
+		return;
+
+	arr[0] = ft_strjoin("_=", value);
+	arr[1] = 0;
+
+	export(1, arr, get_envp_internal(NULL));
 }
