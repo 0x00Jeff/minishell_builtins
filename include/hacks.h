92285a9ec80a36ca3c1959ebe2a2ea91ca0632fb
Abdelhamid Fatimi <jeff_septiceye@protonmail.com>
2024-02-26 20:40:10 +0100

    chore: Edit type in hacks.h

--- include/hacks.h
+++ include/hacks.h
@@ -6,13 +6,13 @@
 /*   By: afatimi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 14:06:42 by afatimi           #+#    #+#             */
-/*   Updated: 2023/10/13 13:42:32 by ylyoussf         ###   ########.fr       */
+/*   Updated: 2024/02/26 20:39:51 by afatimi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HACKS_H
 # define HACKS_H
 
-void	set_exit_status(int exit_status);
+void	set_exit_status(unsigned int exit_status);
 int		get_exit_status(void);
 #endif
